fd9a1e348c28849ae7284a2defac8b4b682ff70c
gitnazarchuk@gmail,com <cosmosloth@Dimas-MacBook-Pro.local>
2015-11-29 03:23:17 +0300

    production setup

--- server/config/deploy.rb
+++ server/config/deploy.rb
@@ -1,19 +1,27 @@
-# config valid only for current version of Capistrano
+# config valid only for Capistrano 3.1
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+# set :rvm_type, :default_environment
+#set :rvm_ruby_version, '2.0.0-p247@my_gemset'
+
+set :rvm_type, :user
+set :rvm_ruby_version, '2.2.3'
+set :rvm_roles, :all
+
+set :application, 'zyvie'
+set :repo_url, 'https://github.com/cosmosloth/zyvie.git'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+ask :branch, 'master'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
-# Default value for :format is :pretty
+set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
+
+set :unicorn_config_path, -> { "#{fetch(:deploy_to)}/current/config/unicorn.rb" }
+
+   # Default value for :format is :pretty
 # set :format, :pretty
 
 # Default value for :log_level is :debug
@@ -23,26 +31,46 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+# set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+ desc 'Configure nginx files'
+ task :configure_nginx do
+   on roles(:web), in: :sequence, wait: 2 do
+     execute "sudo cp #{current_path}/nginx/#{fetch(:application)}_#{fetch(:stage)}.conf /etc/nginx/sites-enabled/ && sudo /etc/init.d/nginx reload"
+   end
+ end
+
+ desc 'Restart application'
+ task :restart do
+   on roles(:app), in: :sequence, wait: 5 do
+     # Your restart mechanism here, for example:
+     # execute :touch, release_path.join('tmp/restart.txt')
+   end
+ end
+
+ after :publishing, :configure_nginx
+
+ after :publishing, :restart
+
+ after :restart, :clear_cache do
+   on roles(:web), in: :groups, limit: 3, wait: 2 do
+     # Here we can do anything such as:
+     # within release_path do
+     #   execute :rake, 'cache:clear'
+     # end
+   end
+ end
 
 end
--- server/config/deploy/production.rb
+++ server/config/deploy/production.rb
@@ -59,3 +59,44 @@
 #     auth_methods: %w(publickey password)
 #     # password: 'please use keys'
 #   }
+server '46.101.240.50',
+       user: 'developer',
+       roles: %w{web app db}
+
+set :rails_env, 'production'
+ask :branch, 'master'
+
+set :deploy_to, '/srv/projects/zyvie'
+
+namespace :deploy do
+
+  def execute_rake(task)
+    within "#{current_path}" do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, task
+      end
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'unicorn:restart'
+    end
+  end
+
+  desc 'Start application'
+  task :start do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'unicorn:start'
+    end
+  end
+
+  desc 'Stop application'
+  task :stop do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'unicorn:stop'
+    end
+  end
+
+end
